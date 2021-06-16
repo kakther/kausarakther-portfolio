f020c8e92e03ce50e03f78cc05b9a390687006cf
Kausar Akther <kausar.akther@gmail.com>
2021-06-16 17:53:46 -0400

    add nav route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import React from 'react'
+
+import Nav from './components/Nav'
+// import Skills from './components/Skills'
+// import Projects from './components/Projects'
+// import Contacts from './components/Contacts'
 
 function App() {
   return (
     <div className="App">
-       <h1>hello</h1>
+       <Nav/>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Nav (){
+    return(
+        <div className="nav">
+            <h3>KA</h3>
+            <ul className="nav-links">
+                <li>about</li>
+                <li>skills</li>
+                <li>projects</li>
+                <li>contact</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Nav
