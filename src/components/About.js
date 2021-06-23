1841c6ba75ca44fcfe54f063dcd281433dcc2256
Kausar Akther <kausar.akther@gmail.com>
2021-06-23 18:18:02 -0400

    add project

--- src/components/About.js
+++ src/components/About.js
@@ -9,10 +9,7 @@ function About() {
             <img id='portfolio-picture' src={picture} />
             <div className="about">
                 <p>
-                    My passion for technology motivated me to leave the <strong>medical</strong> field for technology world 
-                    and become an expert in <strong>software testing</strong>. I strongly believe I will be a good engineer because  
-                    I am truly passionate about solving complex problems. I am a team player who does not compromise with 
-                    quality. I am always eager bring best practices into the table. 
+                    I am an enthusiastic and self-motivated software engineer, who loves challenges and solving complex problems. My mission is to help companies to expand their presence in the digital world as well as the security and risk control fields. I strive to combine my passion for development with quality code in order to exceed both internal and client expectations. I am a team player who does not compromise with quality. I am always eager to bring best practices into the table. 
                 </p>
                 <br />
                 <a href='https://drive.google.com/file/d/1zc5oCjYIjFXh0EwwebQjMtK9mXPxVMDI/view?usp=sharing' rel="noreferrer" target="_blank">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,34 @@ function Projects() {
             <p>Current and Previous Projects</p>
             <hr/>
             <ul className="projects-ul">
+
+                <li>
+                    <a href='https://zarafund.herokuapp.com/fund'><img src="https://i.imgur.com/642ADCD.png"/>
+                    </a>
+                    <h4>zarafund</h4>
+                    <div className='icon'>
+                    <ul className='icon-ul'>
+                    <li>
+                        <a href="https://zarafund.herokuapp.com/fund"><i class="fas fa-external-link-square-alt"></i></a>
+                    </li>
+                    <li >
+                        <a href="https://github.com/"><i class="fab fa-github-square"></i></a>
+                    </li>
+                    </ul>
+                    </div>
+                    <p > A full CRUD app that allows the user to add, edit, and delete orginations.</p>
+                    <ul className='lag'>
+                        <li>JS(EJS)</li>
+                        <li>Node.js</li>
+                        <li>Mongoose</li>
+                        <li>Express</li>
+                        <li>mongoDB</li>
+
+                    </ul>
+
+                </li>
+
+
                 <li>
                     <a href='https://peaceful-caverns-65474.herokuapp.com/'><img src="https://i.imgur.com/nrNrig1.png"/>
                     </a>
@@ -21,7 +49,9 @@ function Projects() {
                     </li>
                     </ul>
                     </div>
-                    <p >We created a full CRUD app that allows the user to add, edit, and delete songs from a playlist.</p>
+                    <p >
+                        We created a full CRUD app that allows the user to add, edit, and delete songs from their playlist. A Full-Stack CRUD SPA built with JS, React, Express, MongoDB, Mongoose and NodeJS. I had the pleasure of collaborating with Ryani Durham on this application.
+                    </p>
                     <ul className='lag'>
                         <li>JavaScript</li>
                         <li>React</li>
